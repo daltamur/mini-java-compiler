e3556b7f26851eb7acb35b40f26b78ad404567d4
daltamur <daltamur@oswego.edu>
2022-10-17 08:49:23 -0400

    fixed type checkins statements boilerplate lol

--- minijavacompiler/src/main/scala/AST_Grammar/typeCheckingVisitor.scala
+++ minijavacompiler/src/main/scala/AST_Grammar/typeCheckingVisitor.scala
@@ -1,67 +1,72 @@
 package AST_Grammar
 
-class typeCheckingVisitor extends ASTVisitor[symbolTable, AST_Grammar.symbolTableVal] {
-  override def visitExpression(expressionVal: expression, a: symbolTable): symbolTableVal = super.visitExpression(expressionVal, a)
+class typeCheckingVisitor extends ASTVisitor[symbolTable, Unit] {
+  private var curError: Option[error] = None
 
-  override def visitGoal(goal: goal, a: symbolTable): symbolTableVal = super.visitGoal(goal, a)
+  def getCurError(): Option[error] = curError
+  override def visit(currentNode: ASTNode, a: symbolTable): Unit = super.visit(currentNode, a)
 
-  override def visitIdentifier(identifier: identifier, a: symbolTable): symbolTableVal = super.visitIdentifier(identifier, a)
+  override def visitExpression(expressionVal: expression, a: symbolTable): Unit = super.visitExpression(expressionVal, a)
 
-  override def visitVarDec(decs: variableDecs, a: symbolTable): symbolTableVal = super.visitVarDec(decs, a)
+  override def visitGoal(goal: goal, a: symbolTable): Unit = super.visitGoal(goal, a)
 
-  override def visitMainClass(clazz: mainClass, a: symbolTable): symbolTableVal = super.visitMainClass(clazz, a)
+  override def visitIdentifier(identifier: identifier, a: symbolTable): Unit = super.visitIdentifier(identifier, a)
 
-  override def visitClass(klass: klass, a: symbolTable): symbolTableVal = super.visitClass(klass, a)
+  override def visitVarDec(decs: variableDecs, a: symbolTable): Unit = super.visitVarDec(decs, a)
 
-  override def visitMethod(method: method, a: symbolTable): symbolTableVal = super.visitMethod(method, a)
+  override def visitMainClass(clazz: mainClass, a: symbolTable): Unit = super.visitMainClass(clazz, a)
 
-  override def visitDataType(dataType: dataType, a: symbolTable): symbolTableVal = super.visitDataType(dataType, a)
+  override def visitClass(klass: klass, a: symbolTable): Unit = super.visitClass(klass, a)
 
-  override def visitBlockStatement(statement: blockStatement, a: symbolTable): symbolTableVal = super.visitBlockStatement(statement, a)
+  override def visitMethod(method: method, a: symbolTable): Unit = super.visitMethod(method, a)
 
-  override def visitIfStatement(statement: ifStatement, a: symbolTable): symbolTableVal = super.visitIfStatement(statement, a)
+  override def visitDataType(dataType: dataType, a: symbolTable): Unit = super.visitDataType(dataType, a)
 
-  override def visitWhileStatement(statement: whileStatement, a: symbolTable): symbolTableVal = super.visitWhileStatement(statement, a)
+  override def visitBlockStatement(statement: blockStatement, a: symbolTable): Unit = super.visitBlockStatement(statement, a)
 
-  override def visitPrintStatement(statement: printStatement, a: symbolTable): symbolTableVal = super.visitPrintStatement(statement, a)
+  override def visitIfStatement(statement: ifStatement, a: symbolTable): Unit = super.visitIfStatement(statement, a)
 
-  override def visitAssignStatement(statement: assignStatement, a: symbolTable): symbolTableVal = super.visitAssignStatement(statement, a)
+  override def visitWhileStatement(statement: whileStatement, a: symbolTable): Unit = super.visitWhileStatement(statement, a)
 
-  override def visitArrayAssignStatement(statement: arrayAssignStatement, a: symbolTable): symbolTableVal = super.visitArrayAssignStatement(statement, a)
+  override def visitPrintStatement(statement: printStatement, a: symbolTable): Unit = super.visitPrintStatement(statement, a)
 
-  override def visitThisExpression(expression: thisExpression, a: symbolTable): symbolTableVal = super.visitThisExpression(expression, a)
+  override def visitAssignStatement(statement: assignStatement, a: symbolTable): Unit = super.visitAssignStatement(statement, a)
 
-  override def visitBooleanExpression(expression: booleanExpression, a: symbolTable): symbolTableVal = super.visitBooleanExpression(expression, a)
+  override def visitArrayAssignStatement(statement: arrayAssignStatement, a: symbolTable): Unit = super.visitArrayAssignStatement(statement, a)
 
-  override def visitIntegerExpression(expression: integerExpression, a: symbolTable): symbolTableVal = super.visitIntegerExpression(expression, a)
+  override def visitThisExpression(expression: thisExpression, a: symbolTable): Unit = super.visitThisExpression(expression, a)
 
-  override def visitCharacterExpression(expression: characterExpression, a: symbolTable): symbolTableVal = super.visitCharacterExpression(expression, a)
+  override def visitBooleanExpression(expression: booleanExpression, a: symbolTable): Unit = super.visitBooleanExpression(expression, a)
 
-  override def visitIdentiferExpression(expression: identifierExpression, a: symbolTable): symbolTableVal = super.visitIdentiferExpression(expression, a)
+  override def visitIntegerExpression(expression: integerExpression, a: symbolTable): Unit = super.visitIntegerExpression(expression, a)
 
-  override def visitNewArrayExpression(expression: newArrayExpression, a: symbolTable): symbolTableVal = super.visitNewArrayExpression(expression, a)
+  override def visitCharacterExpression(expression: characterExpression, a: symbolTable): Unit = super.visitCharacterExpression(expression, a)
 
-  override def visitNewClassInstanceExpression(expression: newClassInstanceExpression, a: symbolTable): symbolTableVal = super.visitNewClassInstanceExpression(expression, a)
+  override def visitIdentiferExpression(expression: identifierExpression, a: symbolTable): Unit = super.visitIdentiferExpression(expression, a)
 
-  override def visitNegatedExpression(expression: negatedExpression, a: symbolTable): symbolTableVal = super.visitNegatedExpression(expression, a)
+  override def visitNewArrayExpression(expression: newArrayExpression, a: symbolTable): Unit = super.visitNewArrayExpression(expression, a)
 
-  override def visitParenthesizedExpression(expression: parenthesizedExpression, a: symbolTable): symbolTableVal = super.visitParenthesizedExpression(expression, a)
+  override def visitNewClassInstanceExpression(expression: newClassInstanceExpression, a: symbolTable): Unit = super.visitNewClassInstanceExpression(expression, a)
 
-  override def visitAndExpression(expression: andExpression, a: symbolTable): symbolTableVal = super.visitAndExpression(expression, a)
+  override def visitNegatedExpression(expression: negatedExpression, a: symbolTable): Unit = super.visitNegatedExpression(expression, a)
 
-  override def visitAddExpression(expression: addExpression, a: symbolTable): symbolTableVal = super.visitAddExpression(expression, a)
+  override def visitParenthesizedExpression(expression: parenthesizedExpression, a: symbolTable): Unit = super.visitParenthesizedExpression(expression, a)
 
-  override def visitCompareExpression(expression: compareExpression, a: symbolTable): symbolTableVal = super.visitCompareExpression(expression, a)
+  override def visitAndExpression(expression: andExpression, a: symbolTable): Unit = super.visitAndExpression(expression, a)
 
-  override def visitSubtractExpression(expression: subtractExpression, a: symbolTable): symbolTableVal = super.visitSubtractExpression(expression, a)
+  override def visitAddExpression(expression: addExpression, a: symbolTable): Unit = super.visitAddExpression(expression, a)
 
-  override def visitMultiplyExpression(expression: multiplyExpression, a: symbolTable): symbolTableVal = super.visitMultiplyExpression(expression, a)
+  override def visitCompareExpression(expression: compareExpression, a: symbolTable): Unit = super.visitCompareExpression(expression, a)
 
-  override def visitArrayLengthExpression(expression: arrayLengthExpression, a: symbolTable): symbolTableVal = super.visitArrayLengthExpression(expression, a)
+  override def visitSubtractExpression(expression: subtractExpression, a: symbolTable): Unit = super.visitSubtractExpression(expression, a)
 
-  override def visitArrayIndexExpression(expression: arrayIndexExpression, a: symbolTable): symbolTableVal = super.visitArrayIndexExpression(expression, a)
+  override def visitMultiplyExpression(expression: multiplyExpression, a: symbolTable): Unit = super.visitMultiplyExpression(expression, a)
 
-  override def visitMethodFunctionCallExpression(expression: methodFunctionCallExpression, a: symbolTable): symbolTableVal = super.visitMethodFunctionCallExpression(expression, a)
+  override def visitArrayLengthExpression(expression: arrayLengthExpression, a: symbolTable): Unit = super.visitArrayLengthExpression(expression, a)
 
-  override def visitNoTail: symbolTableVal = super.visitNoTail
+  override def visitArrayIndexExpression(expression: arrayIndexExpression, a: symbolTable): Unit = super.visitArrayIndexExpression(expression, a)
+
+  override def visitMethodFunctionCallExpression(expression: methodFunctionCallExpression, a: symbolTable): Unit = super.visitMethodFunctionCallExpression(expression, a)
+
+  override def visitNoTail: Unit = super.visitNoTail
 }
