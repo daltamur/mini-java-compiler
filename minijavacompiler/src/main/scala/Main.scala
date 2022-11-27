cc57f9b8175892bb28f35693491c0ea0ffe2995c
daltamur <daltamur@oswego.edu>
2022-11-27 10:08:53 -0500

    minor changes to help screen

--- minijavacompiler/src/main/scala/Main.scala
+++ minijavacompiler/src/main/scala/Main.scala
@@ -88,8 +88,8 @@ object Main {
         "................~?J77!!!~~~~!!!!!!~:...J5:.....:5PJ..:?????J5Y!.:JPJ....~PP~.7J???J557..............\n" +
         "..................^!~:^^^^^^^^:::....:!J~......:5PJ.........?P5:.^PP7...JPJ........!PP^.............\n" +
         ".................:!J?777777777777:.:^~^........:5PJ...^!????YP5:..7P5^.^PP^..:!????JPP^.............\n" +
-        "....................^~:::^^::::................:5PJ..7P5~:::?P5:..:YPJ.JP7..~5P!^::!PP^.............\n" +
-        ".............^~~:..:?YJ????????^......:........:5PJ..JPY^:^~YP5:...~PPYPJ...7P5~::~?PP^.............\n" +
+        "....................^~:::^^::::................:5PJ..7P5    ?P5:..:YPJ.JP7..~5P!    PP^.............\n" +
+        ".............^~~:..:?YJ????????^......:........:5PJ..JPY    YP5:...~PPYPJ...7P5~    PP^.............\n" +
         "............~YPJ~^::::::^^^^::::::^^~7^........:5P?..:7JYJ?7~??:....7JJ?:...:7JYJ?7~?J^.............\n" +
         ".............:^!!777777!!!!!!!!!!!!!!~~^.......!PY^.................................................\n" +
         "...................:^^^^^~~^~~~~~~^^^::.......:7?:..................................................\n" +
@@ -100,13 +100,13 @@ object Main {
       println("Welcome to this Mini Java Compiler!")
       println("____________________________________")
       println("This Compiler uses: ")
-      println("-ANTLR for Parsing. Learn about ANTLR at https://www.antlr.org/")
-      println("-Java ASM for code generation. Learn more about ASM at https://asm.ow2.io/")
+      println("-ANTLR for Parsing. Learn about ANTLR at: https://www.antlr.org/")
+      println("-Java ASM for code generation. Learn more about ASM at: https://asm.ow2.io/")
       println("I used this handy Intellij Plugin to analyze JVM bytecode when writing this compiler: https://plugins.jetbrains.com/plugin/16970-byte-code-analyzer")
       println("____________________________________")
-      println("Learn about how to write in Mini Java at https://www.cambridge.org/resources/052182060X/")
+      println("Learn about how to write in Mini Java at: https://www.cambridge.org/resources/052182060X/")
       println("This version of Mini Java also supports characters and will print them out")
-      println("View the source code for this compiler at https://github.com/daltamur/mini-java-compiler")
+      println("View the source code for this compiler at: https://github.com/daltamur/mini-java-compiler")
       println("To compile a Java file that follows the rules of MiniJava, run this program with the file location as a command line argument")
     }else{
       println("ERROR: Please give ONE filename argument to the compiler. Run with no arguments to see the INFO screen")
